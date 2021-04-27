ec00ea05175e118984d01e29d745b452d2cd6c41
pradeepiOS <pradeep.iostech@gmail.com>
2021-04-27 23:19:54 +0530

    Created new project for circleci demo.

--- CIrlceCIPOCTests/CIrlceCIPOCTests.swift
+++ CIrlceCIPOCTests/CIrlceCIPOCTests.swift
@@ -22,13 +22,10 @@ class CIrlceCIPOCTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+        
+        let i = 0
+        
+        XCTAssertTrue(i==0, "I supposed to be zero")
     }
 
 }
